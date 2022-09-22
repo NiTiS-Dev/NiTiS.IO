ddd458dd14d9018dd4f9f89a0dedbcadb220d419
NickName73 <NickName73@users.noreply.github.com>
2022-09-22 20:16:59 +0300

    Tests fix

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -6,9 +6,9 @@ internal class Program
 	{
 		File file = new("NiTiS.IO.dll");
 
-		Console.WriteLine($"{file:size:KiB}");
-		Console.WriteLine($"{file:size:KiB!0.00 KiB}");
-		Console.WriteLine($"{file:size:MiB!0.0000 MiB}");
-		Console.WriteLine($"{file:size:B!0 Bytes}");
+		Console.WriteLine($"{file:Size:KiB}");
+		Console.WriteLine($"{file:Size:KiB!0.00 KiB}");
+		Console.WriteLine($"{file:Size:MiB!0.0000 MiB}");
+		Console.WriteLine($"{file:Size:B!0 Bytes}");
 	}
 }
